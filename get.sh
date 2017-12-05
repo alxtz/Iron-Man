e8a824263a85953feee8640bb7d7f790328b3fb6
Alex Tzeng <alxtz.tw@gmail.com>
2017-12-05 22:41:28 +0800

    Day 2

---
+++ get.sh
@@ -0,0 +1,1 @@
+curl -X GET "https://graph.facebook.com/v2.6/me/messenger_profile?fields=whitelisted_domains&access_token=EAAE06k1Ba04BAA4VuKSF2TlzNc3uw6stQDvmO3I4PBjXz7NvJZAZBxNzdjTHmhffvrO6GmBFfLG9a8pqdCGRqYUJnsTjKyHd1ENzSIx7DrDtcwjMZCuxb6tyyGJRPsHyjBuW1ZAncwkZCf2u4aWJsh3V16ge6L5XxzfqKOZAbZA3BzgtMPREs20"
--- whitelist.sh
+++ whitelist.sh
@@ -1,10 +1,7 @@
 curl -X POST -H "Content-Type: application/json" -d '{
 
-  "home_url": {
-    "url": "https://news.ycombinator.com/",
-     "webview_height_ratio": "tall",
-     "webview_share_button": "show",
-     "in_test": true
-  }
+  "whitelisted_domains": [
+    "https://news.ycombinator.com/",
+  ]
 
 }' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAE06k1Ba04BAA4VuKSF2TlzNc3uw6stQDvmO3I4PBjXz7NvJZAZBxNzdjTHmhffvrO6GmBFfLG9a8pqdCGRqYUJnsTjKyHd1ENzSIx7DrDtcwjMZCuxb6tyyGJRPsHyjBuW1ZAncwkZCf2u4aWJsh3V16ge6L5XxzfqKOZAbZA3BzgtMPREs20"
